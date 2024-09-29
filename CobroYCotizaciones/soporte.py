9e77734123be1eb91c515839805287d9f12d8d89
Miguel Zamora <miguelzamora249@gmail.com>
2024-09-29 16:23:18 -0700

    catalogo en C# avance conexion a bd  Miguel Zamora

--- CobroYCotizaciones/soporte.py
+++
@@ -1,9 +0,0 @@
-consultas = []
-
-def enviar_consulta(cliente, mensaje):
-    consulta = {"cliente": cliente, "mensaje": mensaje}
-    consultas.append(consulta)
-    return "Consulta enviada correctamente."
-
-def ver_consultas():
-    return consultas
--- obj/Debug/net8.0/catalogo_Agencia.AssemblyInfo.cs
+++ obj/Debug/net8.0/catalogo_Agencia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("catalogo_Agencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa818732814961e58acac593e935c3fda2f73153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb90523ea937dea9a78c7d6a4ef01783f9e12f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("catalogo_Agencia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("catalogo_Agencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
